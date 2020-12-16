a485aab87ad249cd150d3eb5b93c8222160cd0b7
Gianpaolo <pao.barga@gmail.com>
2020-12-16 16:23:32 +0100

    esercizio di base finito

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-var listaCognomi = [‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’];
+var surnamesList = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
+var lowerCaseSurnamesList = []; // bounus: usa map
 
-// recupera cognome
 
-// inserire cognome in listaCognomi
+// recupera cognome e preparalo per la lista (minuscolo)
+    // verifica non ci siano numeri - bonus
+var surname = prompt('Scrivi il tuo cognome');
 
-// ordinare alfabeticamente listaCognomi
+// inserire cognome in surnamesList
+surnamesList.push(surname);
+
+// rendi tutto minuscolo al fine di riordinarli alfabeticamente senza problemi
+for (var i = 0; i < surnamesList.length; i++) {
+    lowerCaseSurnamesList.push(surnamesList[i].toLowerCase()); 
+}
+
+// ordinare alfabeticamente surnamesList
+lowerCaseSurnamesList.sort();
 
 // individuare posizione cognome inserito all'interno della lista
+var userSurnameIndex = lowerCaseSurnamesList.indexOf(surname) + 1;
 
-// creare ul da listaCognomi con posizione cognome inserito
+// creare ul da surnamesList con posizione cognome inserito
+var displayedList = '';
+for (var l = 0; l < lowerCaseSurnamesList.length; l++) {
+    if(lowerCaseSurnamesList[l] === surname) {
+        displayedList += '<li class="user">' + lowerCaseSurnamesList[l] + ', ' + userSurnameIndex + '</li>'
+    } else {
+        displayedList += '<li>' + lowerCaseSurnamesList[l] + '</li>'
+    }
+}
+
+document.querySelector('#list').innerHTML = displayedList;
