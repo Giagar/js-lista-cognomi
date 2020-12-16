8af76e427821b9e180c8d208a9f6d73342e90139
Gianpaolo <pao.barga@gmail.com>
2020-12-16 15:46:20 +0100

    fatto pseudocode

---
+++ script.js
@@ -0,0 +1,11 @@
+var listaCognomi = [‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’];
+
+// recupera cognome
+
+// inserire cognome in listaCognomi
+
+// ordinare alfabeticamente listaCognomi
+
+// individuare posizione cognome inserito all'interno della lista
+
+// creare ul da listaCognomi con posizione cognome inserito
